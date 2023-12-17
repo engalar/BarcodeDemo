ff55cbbde6058ab6ec4dcd041b4ecfa429986423
engalar <engalar@gmail.com>
2023-12-17 20:10:17 +0800

    feat: code edit

--- OpenAndroid.ps1
+++ OpenAndroid.ps1
@@ -1,2 +1,2 @@
 $firstAvd = & "$env:SystemDrive\Android\android-sdk\emulator\emulator.exe" -list-avds | Select-Object -First 1
-& "$env:SystemDrive\Android\android-sdk\emulator\emulator.exe" -avd $firstAvd
+& "$env:SystemDrive\Android\android-sdk\emulator\emulator.exe" -netdelay none -netspeed full -no-snapshot-load -avd $firstAvd
